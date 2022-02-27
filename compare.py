7c56ed8be42a6d3da7733b20dc3d133ced42cc53
xwy964046872 <39191247+xwy964046872@users.noreply.github.com>
2022-02-27 22:01:24 +0800

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,28 @@
+card_type = {
+    'High card':0,
+    'Pair':1,
+    'Two pairs':2,
+    'Three of a kind':3,
+    'Straight':4,
+    'Flush':5,
+    'Full house':6,
+    'Four of a kind':7,
+    'Straight flush':8,
+    'Royal flush':9
+}
+
+
+
+
+hand_card = [[['♦', 'A']], [['♦', '10']]]
+
+table_card = [[['♦', '9']], [['♥', '10']], [['♠', '10']], [['♠', '9']],[['♣', '5']]]
+
+# print(type(card_type))
+
+# print(card_type['High card'])
+
+# test = 'High card'
+#
+# print(card_type[test])
+
